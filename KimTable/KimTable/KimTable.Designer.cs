39741ff8de2d3714427f96238bb80f889fe95746
kim40133846 <45849906+kim40133846@users.noreply.github.com>
2020-02-10 10:29:22 -0300

    Created Arquitecture

---
+++ KimTable/KimTable/KimTable.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace KimTable
+{
+    partial class KimTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.boxUser = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.boxPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(103, 72);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(87, 26);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuario";
+            this.lblUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxUser
+            // 
+            this.boxUser.Location = new System.Drawing.Point(87, 115);
+            this.boxUser.Name = "boxUser";
+            this.boxUser.Size = new System.Drawing.Size(116, 20);
+            this.boxUser.TabIndex = 1;
+            this.boxUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(85, 167);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(125, 26);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Contraseña";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boxPassword
+            // 
+            this.boxPassword.Location = new System.Drawing.Point(87, 211);
+            this.boxPassword.Name = "boxPassword";
+            this.boxPassword.Size = new System.Drawing.Size(116, 20);
+            this.boxPassword.TabIndex = 3;
+            this.boxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.boxPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLogin.Location = new System.Drawing.Point(108, 271);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Entrar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // KimTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(290, 358);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.boxPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.boxUser);
+            this.Controls.Add(this.lblUser);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "KimTable";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KimTable";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox boxUser;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox boxPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ KimTable/KimTable/KimTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KimTable.Common;
+
+namespace KimTable
+{
+    public partial class KimTable : Form
+    {
+        public KimTable()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            WorkTable wt = new WorkTable();
+            this.Visible = false;
+
+            if (boxUser.Text == "Kevin") wt.ShowDialog();
+            else { System.Windows.Forms.MessageBox.Show("ERROR, el usuario ingresado no existe.","No se pudo iniciar sesion"); }
+
+            this.Visible = true;
+        }
+    }
+}
